92663123bdfff6863b4f46576f5de2409b6a5e1b
frederic.vlummens <frederic.vlummens@howest.be>
2022-12-12 13:38:07 +0100

    Add domain layer

---
+++ src/main/java/ui/fx/MovieReviewController.java
@@ -0,0 +1,20 @@
+package ui.fx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MovieReviewController {
+
+    @FXML
+    private void onSearch(ActionEvent e) {
+    }
+
+    @FXML
+    public void onDisplayMovie(ActionEvent e) {
+    }
+
+    @FXML
+    public void onAddReview(ActionEvent e) {
+
+    }
+}
--- src/main/java/ui/fx/Program.java
+++ src/main/java/ui/fx/Program.java
@@ -1,4 +1,34 @@
 package ui.fx;
 
-public class Program {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Program extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = getClass().getResource("/fxml/MovieReview.fxml");
+        Locale locale = new Locale("en", "US");
+
+        ResourceBundle bundle = ResourceBundle.getBundle("MovieReview", locale);
+
+        FXMLLoader loader = new FXMLLoader(url, bundle);
+
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
